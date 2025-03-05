5defd88183376ea51b5f06636cffa8033f888bf8
“Andrew <a.parry.1@research.gla.ac.uk>
2025-03-05 16:00:57 +0000

    fix index

--- scripts/run_indexing.sh
+++ scripts/run_indexing.sh
@@ -4,13 +4,13 @@ IR_DATASET=$3
 CHECKPOINT=$4
 mkdir -p $OUTPUT_DIR
 
-CMD = "python corpuspp.index \
+CMD="python corpuspp.index \
     --index_path $INDEX_PATH \
     --ir_dataset $IR_DATASET \
     --retriever $RETRIEVER"
 
 if [ -n "$CHECKPOINT" ]; then
-    CMD += " --checkpoint $CHECKPOINT"
+    CMD+=" --checkpoint $CHECKPOINT"
 fi
 
 eval $CMD
