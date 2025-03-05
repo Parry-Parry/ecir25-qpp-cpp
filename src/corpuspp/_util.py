66104bf7d64fe3c5b0147319b7781ebc27d564cc
“Andrew <a.parry.1@research.gla.ac.uk>
2025-03-05 16:11:14 +0000

    there we go

--- src/corpuspp/_util.py
+++ src/corpuspp/_util.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 
 def queries_from_jsonl(file: str):
-    return pd.read_json(file, lines=True, orient='records')
+    queries = pd.read_json(file, lines=True, orient='records')
+    queries['qid'] = [str(i) for i in range(len(queries))]
+    return queries
