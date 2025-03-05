fc2a3b28defe0b6d9ea07c318029e29db58522c0
“Andrew <a.parry.1@research.gla.ac.uk>
2025-03-05 16:17:10 +0000

    should help

--- src/corpuspp/_util.py
+++ src/corpuspp/_util.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
 
+def convert_unicode(text):
+    try:
+        return text.encode('utf-8').decode('unicode_escape')
+    except UnicodeDecodeError:
+        return text
+
+
 def queries_from_jsonl(file: str):
     queries = pd.read_json(file, lines=True, orient='records')
+    queries['query'] = queries['query'].apply(convert_unicode)
     queries['qid'] = [str(i) for i in range(len(queries))]
     return queries
